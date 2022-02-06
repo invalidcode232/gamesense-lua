189d5686cc8e7d9d73c2f0520ac01f76f128badb
invalidcode <78639481+invalidcode232@users.noreply.github.com>
2022-02-06 14:57:39 +0700

    Update r_jumpscout.lua

--- r_jumpscout.lua
+++ r_jumpscout.lua
@@ -1,5 +1,8 @@
 local strafe = ui.reference('MISC', 'Movement', 'Air strafe')
 
+local TAB, CONTAINER = 'MISC', 'Movement'
+local threshold_slider = ui.new_slider(TAB, CONTAINER, 'JS velocity threshold', 2, 250, 5, true, 'u', 1, {})
+
 local function get_velocity(ent)
     local vx, vy, vz = entity.get_prop(ent, 'm_vecVelocity')
 
@@ -7,5 +10,5 @@ local function get_velocity(ent)
 end
 
 client.set_event_callback('setup_command', function ()
-    ui.set(strafe, get_velocity(entity.get_local_player()) > 5)
+    ui.set(strafe, get_velocity(entity.get_local_player()) > ui.get(threshold_slider))
 end)
